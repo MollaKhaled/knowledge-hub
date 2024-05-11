23c511e76e9fbbc3c203d5063a55cf9f0720aadc
Mollakhaled <khaledbalok@gmail.com>
2024-05-11 12:39:34 +0600

    title added

--- src/Components/Read/Read.jsx
+++ src/Components/Read/Read.jsx
@@ -4,21 +4,29 @@ import './Read.css';
 const Read = ({ read }) => {
   let totalReadTime = 0;
   for (const item of read) {
-      totalReadTime =totalReadTime + parseInt(item.readTime);
-      
-    }
+    totalReadTime = totalReadTime + item.readTime;
+
+  }
 
   let totalBookMarked = 0;
-    for (const item of read) {
-      totalBookMarked =totalBookMarked + parseInt(item.quantity);
-      
-    }
-    
+  for (const item of read) {
+    totalBookMarked = totalBookMarked + item.quantity;
+
+  }
+
+  let totalBio = '';
+  for (const item of read) {
+    totalBio = totalBio + item.bio + '\n';
+
+  }
+
+
   return (
     <div className='read'>
       <p>Read time: {totalReadTime} min</p>
-      <p>Bookmarked Blogs: {totalBookMarked}</p>
-      
+      <p>Bookmarked Blogs: {totalBookMarked}min</p>
+      <p>Title:<br/> {totalBio}</p> 
+
     </div>
   );
 };
